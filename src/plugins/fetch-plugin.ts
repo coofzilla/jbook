cdfc3940df4e65b14428e9008856a416959524fa
Jay Hernandez <jeric614@yahoo.com>
2021-11-21 14:58:30 +0900

    extrached cache logic

--- src/plugins/fetch-plugin.ts
+++ src/plugins/fetch-plugin.ts
@@ -16,8 +16,9 @@ export const fetchPlugin = (inputCode: string) => {
           contents: inputCode,
         };
       });
-
-      build.onLoad({ filter: /.css$/ }, async (args: any) => {
+      //requires onLoad to return an object
+      //continues until object returned
+      build.onLoad({ filter: /.*/ }, async (args: any) => {
         const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(
           args.path
         );
@@ -25,6 +26,9 @@ export const fetchPlugin = (inputCode: string) => {
         if (cachedResult) {
           return cachedResult;
         }
+      });
+
+      build.onLoad({ filter: /.css$/ }, async (args: any) => {
         const { data, request } = await axios.get(args.path);
 
         const escaped = data
@@ -47,13 +51,6 @@ export const fetchPlugin = (inputCode: string) => {
       });
 
       build.onLoad({ filter: /.*/ }, async (args: any) => {
-        const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(
-          args.path
-        );
-
-        if (cachedResult) {
-          return cachedResult;
-        }
         const { data, request } = await axios.get(args.path);
 
         const result: esbuild.OnLoadResult = {
